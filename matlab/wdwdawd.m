f437563d3a74ffbf11927e2bd77d74f458bda3b3
Sarvesh Mayilvahanan <sarvesh.mayil@gmail.com>
2023-09-28 11:57:52 -0400

    added another controller that has ID

--- matlab/wdwdawd.m
+++ matlab/wdwdawd.m
@@ -2,13 +2,7 @@ clc; clear; close all
 
 load("TestTrack.mat");
 
-X = [287,5,-176,0,2,0]; 
-% X.x = x0(1);
-% X.u = x0(2);
-% X.y = x0(3);
-% X.v = x0(4);
-% X.psi = x0(5);
-% X.r = x0(6); 
+X = [287,5,-176,0,2,0];
 
 figure
 hold on
@@ -16,9 +10,13 @@ plot(TestTrack.bl(1,:), TestTrack.bl(2,:))
 plot(TestTrack.br(1,:), TestTrack.br(2,:))
 trajectory = animatedline(X(1), X(3));
 
+I = 0.0;
+prev_error = 0.0;
+
 verbose = true;
 while true
-    U = braindeadController(X, TestTrack, verbose);
+    [U,I,prev_error] = braindeadController2(X, I, prev_error, TestTrack, verbose);
+    % U = braindeadController(X, TestTrack, verbose);
     [X, T] = forwardIntegrateControlInput(U, X);
     X = X(end,:);
     addpoints(trajectory, X(1), X(3))
